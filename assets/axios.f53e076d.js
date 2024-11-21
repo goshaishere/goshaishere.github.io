4059a05821efbab29ccc689123913bba39f4cc11
Egor Eremin <63669902+goshaishere@users.noreply.github.com>
2024-11-22 00:32:31 +0300

    Add files via upload

---
+++ assets/axios.f53e076d.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.f0e41c56.js";import{a}from"./axios.6b484fa5.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
